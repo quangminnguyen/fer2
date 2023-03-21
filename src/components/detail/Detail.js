de7eac39e4b120e1dfb9bc0d0e4efa1d28e34bb9
ThangPQ <phamthangyn@gmail.com>
2023-03-21 14:10:02 +0700

    detail update

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 const Detail = () => {
-	return <div>Detail</div>;
+  const [movie, setMovie] = useState({
+    id: "",
+    image: "",
+    name: "",
+    type: "",
+    year: "",
+    description: "",
+  });
+  return (
+    <div id="movieDetail">
+      <div className="image">
+        <img alt="movieImage" src={movie.image} />
+      </div>
+      <div className="movieInfor">
+        <h2>{movie.name}</h2>
+        <div className="">
+          <h4>Thể loại: </h4>
+          {movie.type}
+        </div>
+        <div>
+          <h4>Điểm đánh giá: </h4>
+        </div>
+        <div>
+          <h4>Mô tả: </h4> {movie.description}
+        </div>
+        <button className="rateButton">Đánh giá</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
