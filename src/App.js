4ef17c9e90f9a066fad51d08deaf02c314b32d9d
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-03-21 13:45:01 +0700

    udpating

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Login from "./components/login/Login";
 import Home from "./components/homepage/Home";
 import Register from "./components/register/Register";
 import Detail from "./components/detail/Detail";
+import { useState } from "react";
+import dataRoot from "./data.json";
 function App() {
+	const [data, setData] = useState(dataRoot);
 	return (
 		<Router>
 			<div className="App">
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/:slug" element={<Detail />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/login" element={<Login />} />
+					<Route path="/" element={<Home data={data} />} />
+					<Route path="/:slug" element={<Detail data={data} />} />
+					<Route path="/register" element={<Register data={data} />} />
+					<Route path="/login" element={<Login data={data} />} />
 				</Routes>
 			</div>
 		</Router>
