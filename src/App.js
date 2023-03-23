7432128703848b78bc01029f2a3735ad1ff1279d
ThangPQ <phamthangyn@gmail.com>
2023-03-24 02:45:24 +0700

    update detail

--- src/App.js
+++ src/App.js
@@ -10,43 +10,43 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { ToastContainer } from "react-toastify";
 function App() {
-	const [data, setData] = useState(dataRoot);
-	useEffect(() => {
-		if (!localStorage.getItem("movies")) {
-			localStorage.setItem("movies", JSON.stringify(data?.movies));
-		}
-		if (!localStorage.getItem("types")) {
-			localStorage.setItem("types", JSON.stringify(data?.types));
-		}
-		if (!localStorage.getItem("accounts")) {
-			localStorage.setItem("accounts", JSON.stringify(data?.accounts));
-		}
-		if (!localStorage.getItem("reviews")) {
-			localStorage.setItem("reviews", JSON.stringify(data?.reviews));
-		}
-	}, []);
+  const [data, setData] = useState(dataRoot);
+  useEffect(() => {
+    if (!localStorage.getItem("movies")) {
+      localStorage.setItem("movies", JSON.stringify(data?.movies));
+    }
+    if (!localStorage.getItem("types")) {
+      localStorage.setItem("types", JSON.stringify(data?.types));
+    }
+    if (!localStorage.getItem("accounts")) {
+      localStorage.setItem("accounts", JSON.stringify(data?.accounts));
+    }
+    if (!localStorage.getItem("reviews")) {
+      localStorage.setItem("reviews", JSON.stringify(data?.reviews));
+    }
+  }, []);
 
-	const handleClearLocalStorage = () => {
-		localStorage.clear();
-	};
-	return (
-		<Router>
-			<div className="App">
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home data={data} />} />
-					<Route path="/:slug" element={<Detail data={data} />} />
-					<Route path="/register" element={<Register data={data} />} />
-					<Route path="/login" element={<Login data={data} />} />
-				</Routes>
-				<Footer />
-				<ToastContainer style={{ fontSize: "1.5rem" }} />
-				<div onClick={handleClearLocalStorage} title="Clear" className="clear">
-					Clear
-				</div>
-			</div>
-		</Router>
-	);
+  const handleClearLocalStorage = () => {
+    localStorage.clear();
+  };
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home data={data} />} />
+          <Route path="/detail/:slug" element={<Detail data={data} />} />
+          <Route path="/register" element={<Register data={data} />} />
+          <Route path="/login" element={<Login data={data} />} />
+        </Routes>
+        <Footer />
+        <ToastContainer style={{ fontSize: "1.5rem" }} />
+        <div onClick={handleClearLocalStorage} title="Clear" className="clear">
+          Clear
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -9,38 +9,155 @@ const Detail = () => {
     type: "",
     year: "",
     description: "",
+    rate: "",
   });
-  const [type, setType] = useState({
+  const [userComment, setUserComment] = useState({
     id: "",
-    name: "",
+    movie_id: "",
+    rateStar: "",
+    comment: "",
+    account_id: "",
   });
+  const [comments, setComments] = useState([]);
+  const [users, setUsers] = useState([]);
+
   const navigate = useNavigate();
   const { slug } = useParams();
+
   useEffect(() => {
+    var user = JSON.parse(localStorage.getItem("user"));
     var movies = JSON.parse(localStorage.getItem("movies"));
-    if (typeof movies === "undefined") {
-      return navigate("/login");
-    }
-    var movieDetail = movies?.find((m) => m.id === slug);
-    if (typeof movieDetail === "undefined") {
-      return navigate("/login");
-    }
-
-    setMovie({ ...movieDetail });
-  }, [slug]);
-
-  useEffect(() => {
     var types = JSON.parse(localStorage.getItem("types"));
-    if (typeof types === "undefined") {
+    var rates = JSON.parse(localStorage.getItem("reviews"));
+    var accounts = JSON.parse(localStorage.getItem("accounts"));
+    if (
+      typeof movies === "undefined" ||
+      typeof types === "undefined" ||
+      typeof rates === "undefined" ||
+      typeof accounts === "undefined"
+    ) {
       return navigate("/login");
     }
-    var typeDetail = types?.find((m) => m.id === slug);
-    if (typeof typeDetail === "undefined") {
+    var movieDetail = movies?.find((m) => m.id == slug);
+    var typeDetail = types?.find((m) => m.id == slug);
+    if (typeof movieDetail === "undefined" || typeof typeDetail === "undefined") {
       return navigate("/login");
     }
 
-    setType({ ...typeDetail });
-  }, [type]);
+    if (accounts.length === 0 || rates.length === 0) {
+      return;
+    }
+    var account;
+    var commentDetail;
+    var totalRate = 0;
+    var movie_rate = rates
+      ?.filter((e) => e.movie_id === movieDetail.id)
+      ?.filter((e) => {
+        account = accounts.find((a) => a.id === e.account_id);
+        if (account.id === user.id) {
+          setUserComment({ ...e });
+        }
+
+        commentDetail = {
+          id: e.id,
+          rateStar: e.rateStar,
+          comment: e.comment,
+          name: account.username,
+          userID: e.userID,
+        };
+        totalRate = totalRate + e.rateStar;
+        if (e.comment !== "") {
+          return commentDetail;
+        } else {
+          return false;
+        }
+      });
+    var rateAverage = totalRate / movie_rate.length;
+
+    setMovie({ ...movieDetail, rate: rateAverage, type: typeDetail.name });
+    setComments([...movie_rate]);
+    setUsers([...accounts]);
+  }, [navigate, movie.id, slug]);
+
+  const RateFormAppear = () => {
+    var user = JSON.parse(localStorage.getItem("user"));
+
+    if (user === null) {
+      return (
+        <button className="rateButton text" onClick={() => navigate("/login")}>
+          Đánh giá
+        </button>
+      );
+    } else {
+      return (
+        <div className="comments">
+          <h1 className=" textTitle bold">Chi tiết đánh giá:</h1>
+          <form id="CommentForm" onSubmit={() => handleSubmitComment()}>
+            <label htmlFor="starRate" className="text">
+              Điểm đánh giá:
+            </label>
+            <input
+              type="number"
+              name="starRate"
+              id="starRate"
+              className="commentStarRate"
+              value={userComment.rateStar}
+              onChange={(e) => {
+                setUserComment({ ...userComment, starRate: e.target.value });
+              }}
+            ></input>
+            <div className="warn" id="startRateWarn"></div>
+            <div className="text">Bình luận:</div>
+            <textarea
+              type="text"
+              name="commentText"
+              id="commentText"
+              cols="100"
+              rows="5"
+              value={userComment.comment}
+              onChange={(e) => {
+                setUserComment({ ...userComment, comment: e.target.value });
+              }}
+            ></textarea>
+          </form>
+          <button className="rateButton text" type="submit">
+            Đánh giá
+          </button>
+        </div>
+      );
+    }
+  };
+  const handleSubmitComment = () => {
+    var user = JSON.parse(localStorage.getItem("user"));
+
+    if (user === null) {
+      return navigate("/login");
+    }
+    if (userComment.rateStar < 0 || userComment.rateStar > 10) {
+      document.querySelector("#startRateWarn").innerHTML = "Điểm đánh giá cần nằm trong khoảng từ 0 đến 10";
+      return;
+    } else {
+      document.querySelector("#startRateWarn").innerHTML = "";
+    }
+    var newComment = {
+      id: "id",
+      movie_id: movie.id,
+      rateStar: userComment.rateStar,
+      comment: userComment.comment,
+      account_id: user.id,
+    };
+    if (userComment.account_id === "") {
+      var id = users[users.length - 1].id + 1;
+      newComment = { ...newComment, id: id };
+      comments.push(newComment);
+    } else {
+      newComment = { ...newComment, id: userComment.id };
+      var index = comments.findIndex((c) => c.id === id);
+      comments[index] = newComment;
+    }
+    localStorage.setItem("reviews", JSON.stringify(comments));
+    
+  };
 
   return (
     <div id="movieDetail">
@@ -49,32 +166,34 @@ const Detail = () => {
       </div>
       <div className="movieInfor">
         <div className="detailInfor">
-          <h1 className=" textTitle">{movie.name}</h1>
+          <h1 className=" textTitle bold">{movie.name}</h1>
           <div className="inforTech text">
-            <span className="textDesTilte">Thể loại: </span>
-            {type.name}
+            <span className="bold">Thể loại: </span>
+            {movie.type}
           </div>
           <div className="inforTech text">
-            <span className="textDesTilte">Điểm đánh giá: </span>
+            <span className="bold">Điểm đánh giá: </span>
+            {movie.rate}
           </div>
           <div className="inforTech text">
-            <span className="textDesTilte">Mô tả: </span> {movie.description}
+            <span className="bold">Mô tả: </span> {movie.description}
           </div>
-          <button className="rateButton text">Đánh giá</button>
         </div>
+        <RateFormAppear />
         <div className="detailRate">
-          <h1 className=" textTitle">Chi tiết đánh giá</h1>
-          {/* <div className="inforTech text">
-            <span className="textDesTilte">Thể loại: </span>
-            {type.name}
-          </div>
-          <div className="inforTech text">
-            <span className="textDesTilte">Điểm đánh giá: </span>
-          </div>
-          <div className="inforTech text">
-            <span className="textDesTilte">Mô tả: </span> {movie.description}
-          </div>
-          <button className="rateButton text">Đánh giá</button> */}
+          <h1 className=" textTitle bold">Bình luận</h1>
+          <div id="commentMessage"></div>
+          {comments.length !== 0 ? (
+            comments.map((e) => (
+              <div id={e.id} key={e.id} className="text comment">
+                <span className="bold">{e.name}</span>: {e.comment}
+              </div>
+            ))
+          ) : (
+            <div id="commentMessage" className="text">
+              Không có comment cho đến thời điểm hiện tại
+            </div>
+          )}
         </div>
       </div>
     </div>
